737318e01834cf8e06d3ffe0a59fb3e19a9d5f81
Bhavani Bhanu <bhanumallethula@gmail.com>
2022-01-16 11:29:04 +0530

    This is Phase1 Final Project

---
+++ src/com/myproject/LockedMeMain.java
@@ -0,0 +1,14 @@
+package com.myproject;
+
+public class LockedMeMain {
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		menuOptions.printWelcomeScreen("Locker", "Bhavani**");
+		
+		HandleOption.HandleWelcomeScreenInput();
+	}
+}
+
